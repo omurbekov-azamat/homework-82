712682b116e5846158f37872f617c779f36af70c
omurbekov-azamat <avgusttin8@gmail.com>
2023-02-15 18:32:36 +0600

    created method with using bcrypt for making hash for password;

--- api/modules/User.ts
+++ api/modules/User.ts
@@ -1,6 +1,9 @@
 import {Model, model, Schema} from "mongoose";
+import bcrypt from 'bcrypt';
 import {randomUUID} from "crypto";
 import {IUser} from "../types";
+
+const SALT_WORK_FACTOR = 10;
 
 interface IUserMethods {
     generateToken(): void;
@@ -21,6 +24,24 @@ const UserSchema = new Schema<IUser, UserModel, IUserMethods>({
     token: {
         type: String,
         required: true,
+    }
+});
+
+UserSchema.pre('save', async function (next) {
+   if (!this.isModified('password')) {
+       next();
+   }
+
+   const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+   this.password = await bcrypt.hash(this.password, salt);
+
+   next();
+});
+
+UserSchema.set('toJSON', {
+    transform: (doc, ret) => {
+        delete ret.password;
+        return ret;
     }
 });
 
