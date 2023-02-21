f85b077c82e249da062066f2d712958db02ddf06
omurbekov-azamat <avgusttin8@gmail.com>
2023-02-21 23:05:44 +0600

    created middleware with file auth.ts and validation; using auth in post router trackHistories;

--- api/routers/trackHistories.ts
+++ api/routers/trackHistories.ts
@@ -1,30 +1,18 @@
 import express from "express";
 import {Error} from "mongoose";
 import TrackHistory from "../modules/TrackHistory";
-import User from "../modules/User";
+import auth, {RequestWithUser} from "../middleware/auth";
 import {TrackHistoryMutation} from "../types";
 
 
 const trackHistoriesRouter = express.Router();
 
-trackHistoriesRouter.post('/', async (req, res, next) => {
+trackHistoriesRouter.post('/',  auth,async (req, res, next) => {
     if (!req.body.track) {
         return res.status(400).send({error: 'All fields are required'});
     }
-
+    const user = (req as RequestWithUser).user;
     try {
-        const token = req.get('Authorization');
-
-        if (!token) {
-            return res.status(401).send({error: 'No token present'});
-        }
-
-        const user = await User.findOne({token});
-
-        if (!user) {
-            return res.status(401).send({error: 'Wrong token!'});
-        }
-
         const trackHistoryData: TrackHistoryMutation = {
             user: user.id,
             track: req.body.track,
