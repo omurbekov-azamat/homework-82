e62fc9979a694d014009bc38007876ee381386e2
omurbekov-azamat <avgusttin8@gmail.com>
2023-02-25 20:41:47 +0600

    "HW#86 - installed persist, created settings for persist

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
-import {store} from "./app/store";
+import {persistor, store} from "./app/store";
+import {PersistGate} from "redux-persist/integration/react";
 import App from './App';
 
 const root = ReactDOM.createRoot(
@@ -11,8 +12,10 @@ const root = ReactDOM.createRoot(
 
 root.render(
     <Provider store={store}>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
+        <PersistGate persistor={persistor}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </PersistGate>
     </Provider>
 );
