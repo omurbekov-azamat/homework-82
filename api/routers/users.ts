3b031a587ff309d4b67aa709012573e747a1302d
omurbekov-azamat <avgusttin8@gmail.com>
2023-03-16 15:06:20 +0600

    HW#91 - installed: dotenv, google-auth-library; created post router for user /google

--- api/routers/users.ts
+++ api/routers/users.ts
@@ -1,11 +1,15 @@
 import crypto from "crypto";
 import express from "express";
 import {Error} from "mongoose";
-import User from "../modules/User";
-import {imagesUpload} from "../multer";
 import {promises as fs} from "fs";
+import {OAuth2Client} from "google-auth-library";
+import {imagesUpload} from "../multer";
+import config from "../config";
+import User from "../modules/User";
 
 const usersRouter = express.Router();
+
+const client = new OAuth2Client(config.google.clientId);
 
 usersRouter.post('/',  imagesUpload.single('image'), async (req, res, next) => {
     try {
@@ -48,6 +52,47 @@ usersRouter.post('/sessions', async (req, res) => {
         return res.send({message: 'Username and password correct', user});
     } catch (e) {
         return res.status(400);
+    }
+});
+
+usersRouter.post('/google', async (req, res, next) => {
+    try {
+        const ticket = await client.verifyIdToken({
+            idToken: req.body.credential,
+            audience: config.google.clientId,
+        });
+        const payload = ticket.getPayload();
+
+        if (!payload) {
+            return res.status(400).send({error: 'Wrong Google token!'});
+        }
+
+        const email = payload['email'];
+        const googleId = payload['sub'];
+        const displayName = payload['name'];
+        const avatar = payload['picture'];
+
+        if (!email) {
+            return res.status(400).send({error: 'Not enough user data'});
+        }
+
+        let user = await User.findOne({googleId});
+
+        if (!user) {
+            user = new User({
+                username: email,
+                password: crypto.randomUUID(),
+                displayName,
+                googleId,
+                avatar,
+            });
+        }
+
+        user.generateToken();
+        await user.save();
+        return res.send({message: 'Login with Google successful!', user});
+    } catch (e) {
+        return next(e);
     }
 });
 
